142a67a5453f2e1e642d2875f7d35ac090390879
brian lynch <messedup719@gmail.com>
2023-01-18 11:00:44 -0700

    added ability to add items to specific stores, test with api calls

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 stores = [
     {
-        "name": "my store",
+        "name": "store",
         "items": [
             {
                 "name": "chair",
@@ -25,3 +25,14 @@ def create_store():
     new_store = {"name": request_data["name"], "items": []}
     stores.append(new_store)
     return new_store, 201
+
+
+@app.post("/store/<string:name>/item")
+def create_item(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_item = {"name": request_data["name"], "price": request_data["price"]}
+            store["items"].append(new_item)
+            return new_item
+    return {"message": "store not found"}, 404
