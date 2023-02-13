3d20b9eb94ab14d074abcc51332f927a6ab1f17d
brian lynch <messedup719@gmail.com>
2023-02-13 15:27:54 -0700

    added delete store/item, update item and reorganized app structure

--- app.py
+++ app.py
@@ -11,6 +11,14 @@ def get_stores():
     return {"stores": list(stores.values())}
 
 
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        abort(404, message="Store not found.")
+
+
 @app.post("/store")
 def create_store():
     store_data = request.get_json()
@@ -21,46 +29,22 @@ def create_store():
         )
 
     for store in stores.values():
-        if store_data["name"] ==store["name"]:
-            abort(400, message="Store already exists")
+        if store_data["name"] == store["name"]:
+            abort(400, message=f"Store already exists.")
 
     store_id = uuid.uuid4().hex
     store = {**store_data, "id": store_id}
     stores[store_id] = store
-    return store, 201
+    return store
 
 
-@app.post("/item")
-def create_item(name):
-    item_data = request.get_json()
-    # Here not only do we need to validate data exists
-    # but also what type of data.  
-    # For example: Price should be a float
-    if (
-        "price" not in item_data
-        or "store_id" not in item_data
-        or "name" not in item_data
-    ):
-        abort(
-            400,
-            message="Bad Request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.",
-        )
-
-    for item in items.values():
-        if (
-            item_data["name"] == item["name"]
-            and item_data["store_id"] == item["store_id"]
-        ):
-            abort(400, message="Item already exists.")    
-    
-    if item_data["store_id"] not in stores:
-        abort(404, message="Store not found.")
-
-    item_id = uuid.uuid4().hex
-    item = {**item_data, "id": item_id}
-    items[item_id] = item
-
-    return item, 201
+@app.delete("/store/<strin:store_id>")
+def delete_stores(store_id):
+    try:
+        del stores[store_id]
+        return {"message": "Store deleted"}
+    except KeyError:
+        abort(404, message="Store not found")
 
 
 @app.get("/item")
@@ -68,17 +52,66 @@ def get_all_items():
     return {"items": list(items.values())}
 
 
-@app.get("/store/<string:store_id>")
-def get_store(store_id):
-    try:
-        return stores[store_id]
-    except KeyError:
-        abort(404, message="Store not found.")
-
-
 @app.get("/item/<string:item_id>")
 def get_item(item_id):
     try:
         return items[item_id]
+    except KeyError:
+        abort(404, message="Item not found.")
+
+
+@app.post("/item")
+def create_item():
+    item_data = request.get_json()
+    # Here not only we need to validate data exists,
+    # But also what type of data. Price should be a float,
+    # for example.
+    if (
+        "price" not in item_data
+        or "store_id" not in item_data
+        or "name" not in item_data
+    ):
+        abort(
+            400,
+            message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.",
+        )
+        
+    for item in items.values():
+        if (
+            item_data["name"] == item["name"]
+            and item_data["store_id"] == item["store_id"]
+        ):
+            abort(400, message=f"Item already exists.")
+
+    if item_data["store_id"] not in stores:
+        abort(404, message="Store not found.")
+
+    item_id = uuid.uuid4().hex
+    item = {**item_data, "id": item_id}
+    items[item_id] = item
+
+    return item
+
+
+
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message": "Item deleted."}
+    except KeyError:
+        abort(404, message="Item not found.")
+
+
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+    if "price" not in item_data or "name" not in item_data:
+        abort(400, message="Bad request. Ensure 'price', and 'name' are included in the JSON payload.")
+    
+    try:
+        item = items[item_id]
+        item |= item_data
+        return item
     except KeyError:
         abort(404, message="Item not found.")
