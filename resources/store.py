99898d45da040f09ea7c5cd7af7c4ebb9b43b9a9
brian <45504650+network-rascal@users.noreply.github.com>
2023-02-15 15:20:38 -0700

    Feature/marshmallow (#17)
    * initial building of schemas and updates to docker commands
    * added schemas for data validation
    * added schemas for responses and updated documentation

--- resources/store.py
+++ resources/store.py
@@ -2,8 +2,8 @@ import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-
 from db import stores
+from schemas import StoreSchema
 
 
 blp = Blueprint("stores", __name__, description="Operations on stores")
@@ -11,6 +11,7 @@ blp = Blueprint("stores", __name__, description="Operations on stores")
 
 @blp.route("/store/<string:store_id>")
 class Store(MethodView):
+    @blp.response(200, StoreSchema)
     def get(self, store_id):
         try:
             return stores[store_id]
@@ -27,16 +28,13 @@ class Store(MethodView):
 
 @blp.route("/store")
 class StoreList(MethodView):
+    @blp.response(200, StoreSchema(many=True))
     def get(self):
-        return {"stores": list(stores.values())}
+        return stores.values()
 
-    def post(self):
-        store_data = request.get_json()
-        if "name" not in store_data:
-            abort(
-                400,
-                message="Bad request. Ensure 'name' is included in the JSON payload.",
-            )
+    @blp.arguments(StoreSchema)
+    @blp.response(200, StoreSchema)
+    def post(self, store_data):
         for store in stores.values():
             if store_data["name"] == store["name"]:
                 abort(400, message=f"Store already exists.")
